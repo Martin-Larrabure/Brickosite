ecdf15e546555beacd320debad358362b2a1526d
Martin <m.larrabu@gmail.com>
2020-07-24 00:29:32 -0500

    persiste problema con mixin

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,8 @@ class RunAfterCompile {
 
 let cssConfig = {
     test: /\.css$/i,
-    use: ['css-loader?url=false', { loader: 'postcss-loader', options: { plugins: postCSSPlugins } }]
+    enforce: 'pre',
+    use: ['css-loader?url=false','source-map-loader', { loader: 'postcss-loader', options: { plugins: postCSSPlugins } }]
 }
 
 let pages = fse.readdirSync('./app').filter(function (file) {
