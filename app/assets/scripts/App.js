0a5f9ac1ab5ec24b4005e16388ba81acf21e022d
Martin <m.larrabu@gmail.com>
2020-07-30 09:43:35 -0500

    all pages added

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -17,21 +17,21 @@ new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".revela_1"), 75);
 new RevealOnScroll(document.querySelectorAll(".revela_2"), 60);
 new MobileMenu()
-let modal
+//let modal
 
-document.querySelectorAll(".open-modal").forEach(el => {
-    el.addEventListener("click", e => {
-        e.preventDefault()
-        if (typeof modal == "undefined"){
-            import (/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
-                modal = new x.default()
-                setTimeout(()=>modal.openTheModal(),20)
-            }).catch(()=>console.log("there was a problem"))
-        }else{
-            modal.openTheModal()
-        }
-    })
-})
+//document.querySelectorAll(".open-modal").forEach(el => {
+//    el.addEventListener("click", e => {
+//        e.preventDefault()
+//        if (typeof modal == "undefined"){
+//            import (/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+//                modal = new x.default()
+//                setTimeout(()=>modal.openTheModal(),20)
+//            }).catch(()=>console.log("there was a problem"))
+//        }else{
+//            modal.openTheModal()
+//        }
+//    })
+//})
 
 
 if(module.hot){
